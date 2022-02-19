cd39daf1f63aa4075c99b3d6c55eb1afd2a04253
mardi2020 <mardi@khu.ac.kr>
2022-02-19 21:31:06 +0900

    sqlSession 변경

--- src/main/java/com/mardi2020/todoapp/Todo/TodoService.java
+++ src/main/java/com/mardi2020/todoapp/Todo/TodoService.java
@@ -1,6 +1,7 @@
 package com.mardi2020.todoapp.Todo;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,29 +10,37 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TodoService {
 
-    private final TodoMapper todoMapper;
+//    private final TodoMapper todoMapper;
+
+    private final SqlSession sqlSession;
 
     public void insertTodo(WriteTodoDTO todo) {
-        todoMapper.insertTodo(todo);
+//        todoMapper.insertTodo(todo);
+        sqlSession.insert("insertTodo", todo);
     }
 
     public List<TodoDTO> getTodoAll() {
-        return todoMapper.getTodoAll();
+//        return todoMapper.getTodoAll();
+        return sqlSession.selectList("getTodoAll");
     }
 
     public void deleteTodo(long id) {
-        todoMapper.deleteTodo(id);
+//        todoMapper.deleteTodo(id);
+        sqlSession.delete("deleteTodo", id);
     }
 
     public void updateTodo(EditTodoDTO todo) {
-        todoMapper.updateTodo(todo);
+//        todoMapper.updateTodo(todo);
+        sqlSession.update("updateTodo", todo);
     }
 
     public List<DateDTO> getTodoByDate(){
-        return todoMapper.getTodoByDate();
+//        return todoMapper.getTodoByDate();
+        return sqlSession.selectList("getTodoByDate");
     }
 
     public List<TodoDTO> getTodoAllByDate(String date) {
-        return todoMapper.getTodoAllByDate(date);
+//        return todoMapper.getTodoAllByDate(date);
+        return sqlSession.selectList("getTodoAllByDate", date);
     }
 }
